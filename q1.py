647c1aa5277df5a1853efa3590ae776fd3a9cce6
Taylor Allen <taylorallen0913@gmail.com>
2022-10-03 20:53:16 -0700

    q1

--- q1.py
+++ q1.py
@@ -8,3 +8,23 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+
+num_list = []
+num_even_consecutive = 0
+num_clusters = 0
+
+while len(num_list) != 10:
+    num_list.append(int(input("Enter a number: ")))
+
+for num in num_list:
+    if num % 2 == 0:
+        num_even_consecutive += 1
+    else:
+        if num_even_consecutive >= 2:
+            num_clusters += 1
+            num_even_consecutive = 0
+
+if num_even_consecutive >= 2:
+    num_clusters += 1
+
+print(num_clusters)
